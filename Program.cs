60940565c2264e70f5d6c0ffec9bed9bb77a0405
Renehx <146578656+Renehx@users.noreply.github.com>
2025-10-16 22:07:33 -0300

    desafio_sistema_smartphone_v1.0

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ nokia.ReceberLigacao();
 nokia.InstalarAplicativo("Instagram");
 
 Console.WriteLine("\n---------------Smartphone Iphone--------------------");
-Smartphone iphone = new Iphone(numero: "(98)99999-8888", modelo: "Iphone 16", imei: " 111111-11-111111-1", memoria: 256);
+Smartphone iphone = new Iphone(numero: "(98)99999-8888", modelo: "Iphone 16", imei: "111111-11-111111-1", memoria: 256);
 iphone.Ligar();
 iphone.ReceberLigacao();
 iphone.InstalarAplicativo("Facebook");
